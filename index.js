e65c1313814ea50ea4177f522f95b0dd3984594e
shoebabedin <shoeb.abedin@intelli.global>
2024-02-07 16:58:23 +0600

    update

--- index.js
+++ index.js
@@ -1,30 +1,38 @@
+
 require('dotenv').config();
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var cookieParser = require('cookie-parser');
 const jwt = require("jsonwebtoken");
-const app = express();
-const port = process.env.PORT || 3000; // Use PORT environment variable or default to 3000
+const app = express()
+const port = process.env.port
 const route = require('./routes');
 const mongodb = require('./config/mongodb');
 
-// Middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:3000", credentials: true, allowedHeaders: "Access-Control-Allow-Origin" })); // Enable CORS for localhost:3000
-app.use(route);
 
-// MongoDB connection
+app.use(cors({origin:"*"}));
+app.options("*", cors());
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+  next();
+});
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cookieParser())
+app.use(express.json());
+app.use(route)
+
 mongodb();
 
-// Routes
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
 
-// Start the server
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+  console.log(`Example app listening on port ${port}`)
+})
