2569cfa6a578eb5bba4420962b514ac388352770
shoebabedin <shoeb.abedin@intelli.global>
2024-02-07 16:52:06 +0600

    update

--- index.js
+++ index.js
@@ -1,33 +1,30 @@
-
 require('dotenv').config();
 const express = require('express');
-var bodyParser = require('body-parser');
-var cors = require('cors');
-var cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const jwt = require("jsonwebtoken");
-const app = express()
-const port = process.env.port
+const app = express();
+const port = process.env.PORT || 3000; // Use PORT environment variable or default to 3000
 const route = require('./routes');
 const mongodb = require('./config/mongodb');
 
+// Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors({ origin: "http://localhost:3000", credentials: true })); // Enable CORS for localhost:3000
+app.use(route);
 
-app.use(cors());
-app.options("*", cors());
-app.use(cors({ origin: "http://localhost:3000" }));
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use(cookieParser())
-app.use(express.json());
-app.use(route)
-
+// MongoDB connection
 mongodb();
 
-
-
+// Routes
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
+// Start the server
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
