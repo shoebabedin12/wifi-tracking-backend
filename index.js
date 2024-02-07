35abe28ed50ff9363481c83c3ba35581478573a7
shoebabedin <shoeb.abedin@intelli.global>
2024-02-07 17:36:20 +0600

    update

--- index.js
+++ index.js
@@ -4,14 +4,12 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const jwt = require("jsonwebtoken");
-
 const app = express();
-const port = process.env.PORT || 3000; // Use PORT environment variable or default to 3000
+const port = process.env.PORT || 5000; // Use PORT environment variable or default to 3000
 const route = require('./routes');
 const mongodb = require('./config/mongodb');
 
-app.options("*", cors());
-app.use(cors({ origin: ["http://localhost:3000", "https://wifi-tracking-frontend.vercel.app"] }));
+app.use(cors());
 
 
 // Other middleware
